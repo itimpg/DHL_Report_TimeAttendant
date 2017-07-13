31ade3f17fd5d702d5a792e8be6a397db606d0de
iTim <itim@outlook.com>
2017-07-13 23:26:20 +0700

    Init another project

---
+++ DHL.Report.Init/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DHL.Report.Init
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAccess = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBroseAccess = new System.Windows.Forms.Button();
+            this.btnBrowseExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAccess
+            // 
+            this.txtAccess.Enabled = false;
+            this.txtAccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAccess.Location = new System.Drawing.Point(189, 43);
+            this.txtAccess.Name = "txtAccess";
+            this.txtAccess.Size = new System.Drawing.Size(293, 26);
+            this.txtAccess.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Access File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Excel Mapping File: ";
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.Enabled = false;
+            this.txtExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExcel.Location = new System.Drawing.Point(189, 75);
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.Size = new System.Drawing.Size(293, 26);
+            this.txtExcel.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(625, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(713, 43);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(157, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(744, 160);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 52);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start Map";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBroseAccess
+            // 
+            this.btnBroseAccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBroseAccess.Location = new System.Drawing.Point(488, 43);
+            this.btnBroseAccess.Name = "btnBroseAccess";
+            this.btnBroseAccess.Size = new System.Drawing.Size(126, 26);
+            this.btnBroseAccess.TabIndex = 7;
+            this.btnBroseAccess.Text = "Browse ...";
+            this.btnBroseAccess.UseVisualStyleBackColor = true;
+            this.btnBroseAccess.Click += new System.EventHandler(this.btnBroseAccess_Click);
+            // 
+            // btnBrowseExcel
+            // 
+            this.btnBrowseExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseExcel.Location = new System.Drawing.Point(488, 75);
+            this.btnBrowseExcel.Name = "btnBrowseExcel";
+            this.btnBrowseExcel.Size = new System.Drawing.Size(126, 26);
+            this.btnBrowseExcel.TabIndex = 8;
+            this.btnBrowseExcel.Text = "Browse ...";
+            this.btnBrowseExcel.UseVisualStyleBackColor = true;
+            this.btnBrowseExcel.Click += new System.EventHandler(this.btnBrowseExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 240);
+            this.Controls.Add(this.btnBrowseExcel);
+            this.Controls.Add(this.btnBroseAccess);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtExcel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAccess);
+            this.Name = "Form1";
+            this.Text = "Access Excel Mapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAccess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtExcel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBroseAccess;
+        private System.Windows.Forms.Button btnBrowseExcel;
+    }
+}
+
---
+++ DHL.Report.Init/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DHL.Report.Init
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += (s, e) =>
+            {
+#if DEBUG
+                txtAccess.Text = @"‪D:\DHL\iCCard3000.mdb";
+                txtPassword.Text = "168168";
+                txtExcel.Text = @"D:\DHL\attendance_tops.xlsx";
+#endif
+            };
+        }
+
+        private void btnBroseAccess_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Access Files|*.mdb;*.accdb",
+            };
+            var result = dlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtAccess.Text = dlg.FileName;
+            }
+        }
+
+        private void btnBrowseExcel_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Excel Files|*.xls;*.xlsx",
+            };
+            var result = dlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtExcel.Text = dlg.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtExcel.Text) || string.IsNullOrEmpty(txtAccess.Text))
+            {
+                MessageBox.Show("กรุณาเลือกไฟล์");
+                return;
+            }
+
+            string accessFile = txtAccess.Text;
+            string accessPassword = txtPassword.Text;
+            string excelFile = txtExcel.Text;
+        }
+    }
+}
